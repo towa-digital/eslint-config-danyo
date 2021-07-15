da7b7c921fd05839647c0abf837e18f90be65840
Matthias Frank <matthias.frank@towa.at>
2021-07-15 15:34:57 +0200

    chore: updated rules

--- rules/base.js
+++ rules/base.js
@@ -42,7 +42,7 @@ module.exports = {
     'no-unused-vars': [
       1,
       {
-        ignoreSiblings: true,
+        ignoreRestSiblings: true,
         argsIgnorePattern: 'res|next|^err', // this mostly targets for node/express apps
       },
     ],
