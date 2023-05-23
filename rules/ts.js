aea36391e4befefb8a167732c19ab5c42436f026
Thai Phan <minh-thai.phan@towa.at>
2023-05-23 14:15:21 +0200

    migrate into new pretier version

--- rules/ts.js
+++ rules/ts.js
@@ -1,9 +1,5 @@
 module.exports = {
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier', 'plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
   settings: {
     'import/parsers': {
